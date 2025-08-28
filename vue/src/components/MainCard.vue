d40dde306b9329a10062c56c6a3a02cbf2af7194
Egor <asd141299@gmail.com>
2025-08-28 15:33:56 +0300

    ...

--- vue/src/components/MainCard.vue
+++ vue/src/components/MainCard.vue
@@ -1,7 +1,49 @@
 <template>
-  <h1>asd</h1>
+  <div class="" @click="openModal">
+    <div class="">
+      <img :src="meal.strMealThumb" :alt="meal.strMeal" loading="lazy">
+      <div class="">{{ meal.strCategory }}</div>
+    </div>
+
+    <div class="">
+      <h3 class="">{{ meal.strMeal }}</h3>
+      <p class="">{{ meal.strArea }}</p>
+
+      <div class="">
+        <span
+        v-for="tag in tags"
+        :key="tag"
+        >
+          {{ tag }}
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { computed } from 'vue';
+
+export default {
+  name: 'MealCard',
+  props: {
+    meal: {
+      type: Object,
+      required: true,
+    }
+  },
+  emits: ['meal-selected'],
+  setup(props, {emit}) {
+    const tags = computed(() => {
+      return props.meal.strTags ? props.meal.strTags.split(",") : []
+    })
+
+    const openModal = () => {
+      emit('meal-selected', props.meal)
+    }
+    return {
+      tags, openModal,
+    }
+  }
+}
 </script>
--- vue/src/components/RecipeList.vue
+++ vue/src/components/RecipeList.vue
@@ -1,8 +1,50 @@
 <template>
-  <Card></Card>
+  <div>
+    <div v-if="loading">
+      <div v-for="n in 8" :key="n">
+        <div></div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+
+    <div v-else-if="Error">
+      <p>{{ error }}</p>
+      <button @click="$emit('retry')">Повторить попытку</button>
+    </div>
+
+    <div v-else-if="meals.length === 0">
+      <p>Рецепты не найдены</p>
+    </div>
+
+    <div v-else>
+      <Card v-for="meal in meals" :key="meal.idMeal" :meal="meal" @meal-selected="$emit('meal-selected', $event)"></Card>
+    </div>
+  </div>
 </template>
 
 <script>
 import Card from './MainCard.vue'
-export default { components: { Card } }
+export default {
+  name: 'MealCard',
+  components: { Card },
+  props: {
+    meals: {
+      type: Array,
+      default: () => []
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: ['meal-selected', 'retry']
+
+}
 </script>
